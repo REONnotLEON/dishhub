25a1bf6a63fe17f83c68e39fa4e334ad5f50e898
Matt <thankyoureo0n@gmail.com>
2024-12-16 18:34:40 +0900

    create SearchBar component

--- dishhub/src/App.js
+++ dishhub/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import RecipeItem from "./components/recipeItem/RecipeItem";
 import HamburgerMenu from "./components/hamburgerMenu/HamburgerMenu";
+import SearchBar from "./components/searchBar/SearchBar";
 
 export default function App (){
     return (
         <>
             <header>
                 <HamburgerMenu />
+                <SearchBar />
             </header>
             <main>
                 <RecipeItem />
---
+++ dishhub/src/components/searchBar/SearchBar.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import SearchGlass from '../../assets/icons/magnifying-glass-solid.svg';
+
+export default function SearchBar () {
+
+    const [query, setQuery] = useState('');
+
+    const handleChange = (e) => {
+        setQuery(e.target.value);
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form className="search-bar" onSubmit={handleSearch}>
+            <div className="search-container">
+                <img
+                    src={SearchGlass}
+                    alt="search-glass icon"
+                    className="search-glass-icon"
+                />
+                <input 
+                    type="text"
+                    className="search-input"
+                    placeholder="Search Recipe"
+                    value={query}
+                    name="search-input"
+                    onChange={handleChange}
+                />
+                <hr/>
+            </div>
+        </form>
+    )
+}
