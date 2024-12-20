55d59927797c4857586cd57f40eca62aea405acb
Matt <thankyoureo0n@gmail.com>
2024-12-20 20:59:44 +0900

    change AuthButtons to userIcon in Header

--- dishhub/src/App.js
+++ dishhub/src/App.js
@@ -6,7 +6,7 @@ import Auth from "./pages/Auth";
 export default function App (){
     return (
         <>
-            <Auth />
+            <Home />
         </>
     )
 }
--- dishhub/src/components/authButtons/AuthButtons.js
+++ dishhub/src/components/authButtons/AuthButtons.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import userIcon from '../../assets/icons/user-solid.svg';
 
 export default function AuthButtons(){
     
     return (
         <div className="auth-container">
-            <button className="login-button">
-                Log in
-            </button>
-            <button className="signup-button">
-                Sign up
-            </button>
+            <img
+             src={userIcon}
+             alt="user-icon"
+             className='user-icon'
+            />
         </div>
     );
 }
